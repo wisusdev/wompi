5122b83d0d172f1d4ed9412e214fd498ed7422f1
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-09-30 08:37:58 -0600

    Vue Import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import './bootstrap';
 
-import {createApp} from "vue";
-import App from './components/App.vue';
-createApp(App).mount("#app");
+import { createApp } from 'vue';
+import assets from './mixins/assets.js';
+import ItemComponent from "./components/ItemComponent.vue";
+
+const app = createApp({});
+app.mixin(assets);
+app.component('ItemComponent', ItemComponent);
+app.mount('#app');
