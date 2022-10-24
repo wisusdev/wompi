81dd94137c4385b5c3a3ddd114f494fef5ee4de8
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-10-23 23:17:58 -0600

    Mejoras a vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="d-flex justify-content-center align-items-center vh-100">
+<div class="container my-5">
     <div class="container">
         @foreach (['danger', 'warning', 'success', 'info'] as $key)
             @if(Session::has($key))
@@ -11,30 +11,12 @@
             @endif
         @endforeach
 
-        <form action="{{route('pay.wompi')}}" method="POST" id="paymentForm">
-            @csrf
-            <div class="card">
-                <div class="card-header">
-                    <img src="{{asset('images/logo.svg')}}" alt="logo">
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-3 mb-3">
-                            <item-component></item-component>
-                        </div>
-                        <div class="col-md-9 mb-3">
-                            <app-component></app-component>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer d-flex justify-content-between align-items-center">
-                    <p class="m-0">Laravel v{{ Illuminate\Foundation\Application::VERSION }} <b>(PHP v{{ PHP_VERSION }})</b></p>
-                    <div>
-                        <button type="submit" class="btn btn-primary">Pagar</button>
-                    </div>
-                </div>
+        <form-component></form-component>
+        <div class="border p-3 rounded mb-3">
+            <div class="d-flex align-items-center">
+                <p class="m-0">Laravel v{{ Illuminate\Foundation\Application::VERSION }} <b>(PHP v{{ PHP_VERSION }})</b></p>
             </div>
-        </form>
+        </div>
     </div>
 </div>
 @endsection
