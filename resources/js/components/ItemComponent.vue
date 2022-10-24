9a095497f9d3a2c90b517c4d56d13958ae4dd377
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-10-23 23:19:55 -0600

    Item component

--- resources/js/components/ItemComponent.vue
+++ resources/js/components/ItemComponent.vue
@@ -3,7 +3,7 @@
 		<img :src="asset('images/product.jpeg')" class="img-fluid" alt="product">
 		<div class="card-body">
 			<label for="monto" class="form-label">¿Cuánto quieres pagar?</label>
-			<input type="number" class="form-control" min="5" step="0.01" name="monto" :value="ramdomNumber" id="monto" required>
+			<input type="number" class="form-control" step="0.01" v-model="amount" id="monto" required>
 		</div>
 	</div>
 </template>
@@ -11,15 +11,18 @@
 <script>
 	export default {
 		name: "ItemComponent",
-		data: () => ({
-			ramdomNumber: 0
-		}),
-		created() {
-			this.ramdomNumber = Math.floor(Math.random() * (10000 - 100) + 100) / 100;
-		},
-		methods: {
 
-		}
+		data: function () {
+			return {
+				amount: 0
+			}
+		},
+
+		mounted() {
+			this.amount = Math.floor(Math.random() * (10000 - 100) + 100) / 100;
+
+			this.$emit('payAmount', this.amount);
+		},
 	}
 </script>
 
