e7bd0c11e509c767c843fc7d087a87a12cde4ec1
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-10-23 23:18:08 -0600

    csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	<meta charset="utf-8">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Wompi</title>
 	<link rel="stylesheet" href="{{asset('css/app.css')}}">
