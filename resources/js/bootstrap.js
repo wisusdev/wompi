e1f2634cf2026791ec2023407cc41786894338b2
Jesus Avelar <jesus.avelar.nav@gmail.com>
2022-10-23 23:18:36 -0600

    csrf and sweetalert2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,8 @@ window.Popper = Popper;
 
 import 'bootstrap';
 
+import Swal from "sweetalert2";
+window.Swal = Swal;
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -16,6 +18,20 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+	window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+	console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
